c8e3cf36ec895a9169fb7931043eeabe499971af
ououou <liuyuqi1030@outlook.com>
2026-05-31 03:15:49 +0800

    优化：为Users表高频查询字段添加数据库索引

--- tm-backend/app/core/models/users.py
+++ tm-backend/app/core/models/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Float, VARCHAR, Sequence, DateTime
+from sqlalchemy import Column, Integer, Float, VARCHAR, Sequence, DateTime, Index
 from app.database import Base
 
 class Users(Base):
@@ -8,17 +8,17 @@ class Users(Base):
     username = Column(VARCHAR(200), nullable=False)
     email = Column(VARCHAR(200), nullable=False)
     password = Column(VARCHAR(500), nullable=True)
-    phone = Column(VARCHAR(80))
+    phone = Column(VARCHAR(80), index=True)
     gender = Column(VARCHAR(32))
     location = Column(VARCHAR(32))
     bumen = Column(VARCHAR(32))
-    role = Column(VARCHAR(32))
+    role = Column(VARCHAR(32), index=True)
     shuzhi = Column(Float, default=0.0)
     learn_hour = Column(Float, default=0.0)
     create_hour = Column(Float, default=0.0)
     avantar = Column(VARCHAR(256))
     desc = Column(VARCHAR(1000))
-    register_time = Column(DateTime)
+    register_time = Column(DateTime, index=True)
     last_login_time = Column(DateTime)
     last_study_time = Column(DateTime)  # 最后学习时间
 
